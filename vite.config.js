7090c937a7d17f5c12cd7b8cdce172736beea2cf
Anaks-CT <anaksthayyil30@gmail.com>
2023-08-02 13:54:50 +0530

    [D[C[C[C[A

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/YOUR_REPOSITORY_NAME",
+  base: "/3D-Threejs-Portfolio",
   plugins: [react()],
   server: {
     host: true,
