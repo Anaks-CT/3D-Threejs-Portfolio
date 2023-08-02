fc76ba387416f4c8ebf2824581d3d0bd2483d329
Anaks-CT <anaksthayyil30@gmail.com>
2023-08-02 15:09:45 +0530

    removed the base url in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/3D-Threejs-Portfolio",
+  // base: "/3D-Threejs-Portfolio",
   plugins: [react()],
   server: {
     host: true,
